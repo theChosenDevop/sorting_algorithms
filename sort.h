a223c524b39bc397b1c6ba989020a65cc3b49afb
jackmarley254 <engndiritu1@gmail.com>
2023-11-21 22:32:40 +0300

    Debugging task 0

--- sort.h
+++ sort.h
@@ -3,17 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
-
-/**
-  * enum bool - Enumeration of Boolean values.
-  * @false: Equals 0.
-  * @true: Equals 1.
-  */
-typedef enum bool
-{
-	false = 0,
-	true 
-} bool;
+#include <stdbool.h>
 
 /**
  * struct listint_s - Doubly linked list node
