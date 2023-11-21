eec9e48022095a79b2296b66cb0b2237dc8c5a89
jackmarley254 <engndiritu1@gmail.com>
2023-11-21 21:34:06 +0300

    Task 0 file

---
+++ 0-bubble_sort.c
@@ -0,0 +1,44 @@
+#include "sort.h"
+
+/**
+  * swap - Swap two integers in array.
+  *@a: The first integer to swap.
+  *@b: The second integer to swap.
+  */
+void swap(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+  * bubble_sort - Sort ann array of integers in ascending order.
+  * @array: An array of integers to sort.
+  * @size: The size of the array.
+  *
+  * Description: Prints the array after each swap.
+  */
+void bubble_sort(int *array, size_t size)
+{
+	int i, j;
+	bool swaped;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = false;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[i], &array[j + 1]);
+				swapped = true;
+			print_array(array, size);
+			}
+		}
+		if (swapped == false)
+			break;
+	}
+}
