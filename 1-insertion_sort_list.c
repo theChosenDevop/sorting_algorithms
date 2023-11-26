ffd898f09147f7a49b793643dbaa4ea239d51433
jackmarley254 <engndiritu1@gmail.com>
2023-11-26 05:48:47 +0300

    Corrections

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,6 +1,27 @@
 #include "sort.h"
 
 /**
+ * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
+ * @h: A pointer to the head of the doubly-linked list.
+ * @n1: A pointer to the first node to swap.
+ * @n2: The second node to swap.
+ */
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+{
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
+	else
+		*h = n2;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
+}
+
+/**
  * insertion_sort_list - sorts a doubly linked list of integers
  *			in ascending order
  * @list: doubly linked list of integers
@@ -10,35 +31,16 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *curr, *temp, *sorted;
 
-	if (*list == NULL || (*list)->next == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-
-	sorted = NULL;
-	while (*list != NULL)
+	for (curr = (*list)->next; curr != NULL; curr = temp)
 	{
-		curr = *list;
-		*list = (*list)->next;
-
-		if (sorted == NULL || curr->n < sorted->n)
+		temp = curr->next;
+		sorted = curr->prev;
+		while (sorted != NULL && curr->n < sorted->n)
 		{
-			curr->next = sorted;
-			curr->prev = NULL;
-			if (sorted != NULL)
-				sorted->prev = curr;
-			sorted = curr;
-		}
-		else
-		{
-			temp = sorted;
-			while (temp->next != NULL && temp->next->n < curr->n)
-				temp = temp->next;
-			curr->next = temp->next;
-			if (temp->next != NULL)
-				temp->next->prev = curr;
-			curr->prev = temp;
-			temp->next = curr;
+			swap_nodes(list, &sorted, curr);
+			print_list((const listint_t *)*list);
 		}
 	}
-	print_list(sorted);
-	*list = sorted;
 }
--- sort.h
+++ sort.h
@@ -26,5 +26,6 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2);
 
 #endif
