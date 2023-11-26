b92dc520760f29c69c5505e6e6b75bdb2e028eb6
jackmarley254 <engndiritu1@gmail.com>
2023-11-26 21:45:31 +0300

    Shell sort - Knuth Sequence

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,12 +1,13 @@
 #include "sort.h"
 
 /**
-shell_sort - Sorts an array of integers in ascending order using Shell sort.
-* @array: The array to be sorted.
-* @size: The size of the array.
-*/
+  *shell_sort - Sorts an array of integers in ascending order using shell sort.
+  *@array: The array to be sorted.
+  *@size: The size of the array.
+  */
 
-void shell_sort(int *array, size_t size) {
+void shell_sort(int *array, size_t size)
+{
 	size_t gap = 1, i, j;
 	int temp;
 
@@ -14,21 +15,23 @@ void shell_sort(int *array, size_t size) {
 	while (gap < size / 3)
 		gap = gap * 3 + 1;
 
-	while (gap > 0) {
+	while (gap > 0)
+	{
 		/* Perform insertion sort for elements at a specific gap */
-		for (i = gap; i < size; i++) {
+		for (i = gap; i < size; i++)
+		{
 			temp = array[i];
 
-			for (j = i; j >= gap && array[j - gap] > temp; j -= gap) {
+			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
 				array[j] = array[j - gap];
-			}
 
 			array[j] = temp;
 		}
 
 		/* Print the array after each pass with the current gap */
-		printf("Gap %lu: ", gap);
-		for (i = 0; i < size; i++) {
+		printf("%lu", gap);
+		for (i = 0; i < size; i++)
+		{
 			printf("%d", array[i]);
 			if (i < size - 1)
 				printf(", ");
